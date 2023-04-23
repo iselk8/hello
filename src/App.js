7b32ef21c6c148a925bbd2084c5677ed4b2dd003
ismail <iselk8@gmail.com>
2023-04-23 10:35:41 -0400

    starting to add css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
 import Employee from "./components/Employee";
+import { useState } from "react";
 
 function App() {
+  const [role, setRole] = useState('dev');
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? (
         <>
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
+          <input type="text" onChange={(e) =>{
+            console.log(e.target.value)
+            setRole(e.target.value);
+          }}/>
+          <Employee name="Ismail" role="intern"/>
+          <Employee name="Houssein" role={role}/>
+          <Employee name="Chloe"/>
         </>
       ) : (
         <p>This is false</p>
