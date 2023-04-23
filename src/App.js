0006d090762eab0837dfeed7be68cc87ef1c3857
ismail <iselk8@gmail.com>
2023-04-23 14:01:18 -0400

    Revert "starting to add css"
    This reverts commit 7b32ef21c6c148a925bbd2084c5677ed4b2dd003.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import "./App.css";
 import Employee from "./components/Employee";
-import { useState } from "react";
 
 function App() {
-  const [role, setRole] = useState('dev');
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? (
         <>
-          <input type="text" onChange={(e) =>{
-            console.log(e.target.value)
-            setRole(e.target.value);
-          }}/>
-          <Employee name="Ismail" role="intern"/>
-          <Employee name="Houssein" role={role}/>
-          <Employee name="Chloe"/>
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
         </>
       ) : (
         <p>This is false</p>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,9 +1,5 @@
-function Employee(props) {
-  return (
-    <>
-      <h3>Employee {props.name}</h3>
-      <p>{props.role ? props.role : 'No role'}</p>
-    </>
-  );
+function Employee(){
+    return <h3>Here is an employee</h3>
+
 }
 export default Employee;
